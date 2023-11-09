c6223db856db49ae41f84f1c481b6ae22dcde9a4
kakalak33 <tu.tran@enotion.io>
2023-11-09 20:32:14 +0700

    - follow reducer section

--- My-works/react-quiz/src/App.js
+++ My-works/react-quiz/src/App.js
@@ -1,7 +1,56 @@
+import { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter.js";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Loader from "./Loader.js";
+import Error from "./Error.js";
+import StartScreen from "./StartScreen.js";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading'
+};
+
+function reducer(state, action) {
+
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready'
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error'
+      }
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 function App() {
-  return <DateCounter />;
+
+  const [{ status, questions }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(err => dispatch({ type: 'dataFailed' }));
+  }, []);
+  return <div className="app">
+    <Header />
+    <Main>
+      {status === 'loading' && <Loader />}
+      {status === 'error' && <Error />}
+      {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+    </Main>
+  </div>;
 }
 
 export default App;
---
+++ My-works/react-quiz/src/Main.js
@@ -0,0 +1,7 @@
+function Main({children}) {
+    return <main>
+        {children}
+    </main>
+}
+
+export default Main;
